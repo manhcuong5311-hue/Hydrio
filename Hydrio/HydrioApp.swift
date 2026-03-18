7e2eb36d083e24b1249b0017a39b24c13b6f6f36
SamCorp <Manhcuong5311@gmail.com>
2026-03-18 18:00:13 +0700

    Initial Commit

---
+++ Hydrio/HydrioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrioApp.swift
+//  Hydrio
+//
+//  Created by Sam Manh Cuong on 18/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HydrioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
